18fa7a5bc5e77af53030537aa951a682d895ff81
Aaron Tagliaboschi <aaron.tagliaboschi@gmail.com>
2014-01-21 22:58:22 -0600

    initial for clh2g3

---
+++ clh2g3ta3/origintest.f
@@ -0,0 +1,17 @@
+      implicit double precision (a-h,o-z)
+      dimension r(3), e(3)
+      
+      data r /2.0,1.5,3.0/
+      data e /0.0,0.0,0.0/
+c nativetest for ClH2g3ta4
+
+c 2, 1.5, 3
+
+      call prepot
+      call pot(r,e)
+      
+      write (6,*) "e(1) is ", e(1)
+      write (6,*) "e(2) is ", e(2)
+      write (6,*) "e(3) is ", e(3)
+      
+      end
